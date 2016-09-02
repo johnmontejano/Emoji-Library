e70d9918adca5291ab2f9613916dfcd9eba07662
johnmontewhat <us4john@gmail.com>
2016-09-01 20:33:27 -0700

    working on displaying emojis to another View Controller

--- EmojiLibrary/ViewController.swift
+++ EmojiLibrary/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var emojiTableVIew: UITableView!
+    var emojis = ["💤", "💯", "🇲🇽", "🌲", "😁", "💯", "😊"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var favSports = ["dude"]
+        
+        emojiTableVIew.dataSource = self
+        emojiTableVIew.delegate = self
+    }
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return emojis.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        print(indexPath.row)
+        let cell = UITableViewCell()
+        cell.textLabel?.text = emojis[indexPath.row]
+       return cell
     }
 
     override func didReceiveMemoryWarning() {
